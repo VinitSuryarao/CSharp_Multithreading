6e24ae52db94562b62b035a3b75b7213424e5ae0
Vinit Suryarao <60574327+VinitSuryarao@users.noreply.github.com>
2020-02-08 17:49:53 +0530

    Add files via upload

---
+++ Multithreading/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace Multithreading
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MultithreadingDemo obj = new MultithreadingDemo();
+
+            // This is Synchoronus Style
+            //obj.Function1();
+            //obj.Function2();
+
+            // This is Asynchoronus Style
+            Thread tObj1 = new Thread(obj.Function1);
+            Thread tObj2 = new Thread(obj.Function2);
+            tObj1.Start();
+            tObj2.Start();
+
+
+            Console.ReadLine(); 
+        }
+    }
+
+    public class MultithreadingDemo
+    {
+        public void Function1()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Function 1 Value = {0}", i);
+                Thread.Sleep(2000);
+            }
+        }
+
+        public void Function2()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Function 2 Value = {0}", i);
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
